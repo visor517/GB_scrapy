32633bf928e8c5315f10f196b6894e8b29a1d98a
visor517 <visor517@yandex.ru>
2022-02-10 15:32:20 +0300

    work in class

--- jobparser/settings.py
+++ jobparser/settings.py
@@ -12,26 +12,29 @@ BOT_NAME = 'jobparser'
 SPIDER_MODULES = ['jobparser.spiders']
 NEWSPIDER_MODULE = 'jobparser.spiders'
 
+LOG_ENABLED = True
+LOG_LEVEL = "DEBUG"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'jobparser (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 ' \
+             'Safari/537.36 '
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
--- jobparser/spiders/hhru.py
+++ jobparser/spiders/hhru.py
@@ -1,10 +1,24 @@
 import scrapy
+from scrapy.http import HtmlResponse
 
 
 class HhruSpider(scrapy.Spider):
     name = 'hhru'
     allowed_domains = ['hh.ru']
-    start_urls = ['http://hh.ru/']
+    start_urls = ['https://hh.ru/search/vacancy?area=1&fromSearchLine=true&text=Python&from=suggest_post&items_on_page=20',
+                  'https://hh.ru/search/vacancy?area=2&fromSearchLine=true&text=Python&from=suggest_post&items_on_page=20']
 
-    def parse(self, response):
-        pass
+    def parse(self, response: HtmlResponse):
+        next_page = response.xpath("//a[@data-qa='pager-next']/@href").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+        links = response.xpath("//a[@data-qa='vacancy-serp__vacancy-title']/@href").getall()
+        for link in links:
+            yield response.follow(link, callback=self.vacancy_parse)
+
+    def vacancy_parse(self, response: HtmlResponse):
+        name = response.xpath("//h1//text()").get()
+        salary = response.xpath("//div[@data-qa='vacancy-salary']//text()").getall()
+        url = response.url
+        print(name)
